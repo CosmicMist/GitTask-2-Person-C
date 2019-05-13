1df8ac4021e42029dfddb1685635aa609bc299d6
CosmicMist <50112919+CosmicMist@users.noreply.github.com>
2019-05-13 11:17:22 -0400

    Added the range generator and text doc

---
+++ GitGud/src/GenerateRange.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author TuckerKA20
+ * @version 5/13/19
+ */
+public class GenerateRange
+{
+    public static void main( String args[] )
+    {
+        Scanner scan = null;
+        
+        try
+        {
+            File f = new File( "./src/range.txt" );
+            scan = new Scanner( f );
+            
+            genRange( scan );
+        }
+        catch ( Exception e )
+        {
+            
+        }
+        finally
+        {
+            if ( scan != null )
+            {
+                
+            }
+        }
+    }
+    
+    public static void genRange( Scanner scan )
+    {
+        int first = 0;
+        int last = 0;
+        
+        while ( scan.hasNext() )
+        {
+            first = scan.nextInt();
+            last = scan.nextInt();
+        }
+        
+        int random = (int)((Math.random()* last) + first);
+        
+        System.out.println( random );
+        
+    }
+}
